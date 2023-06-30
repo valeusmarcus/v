4229a7bcee8856674f207a75db8f92f059eeea33
valeusmarcus <138186955+valeusmarcus@users.noreply.github.com>
2023-06-29 21:51:12 -0700

    Add files via upload

---
+++ original reseaux sociale/app.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".menu-hambeger");
+const topbarnav =document.querySelector(".topbar-nav")
+    
+burger.addEventListener("click", () =>{
+    topbarnav.classList.toggle('mobil-menu')
+
+})
